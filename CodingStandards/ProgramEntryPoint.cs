c44648de60ae05b47fc86069e0e00de091f6b783
Brennan McElhanon <bmcelhanon@mcelhanon.com>
2017-01-23 10:29:00 -0700

    Updated ProgramEntryPoint.cs to comply with coding standards.

--- CodingStandards/ProgramEntryPoint.cs
+++ CodingStandards/ProgramEntryPoint.cs
@@ -5,33 +5,48 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+/// <summary>
+/// Writes a celebratory new year message.
+/// </summary>
 class Program
 {
-        //This is the entry point for the program.
-        static int Main(string[] args)
-{
-    return writeMessage(dtNow, Console.Out);
-}
+    private static DateTime Now = DateTime.Now;
 
-        static DateTime dtNow = DateTime.Now;
+    // This is the entry point for the program.
+    /// <summary>
+    /// Prints five new year messages.
+    /// </summary>
+    /// <param name="args">Arguments for the application.</param>
+    /// <returns>The integer 0.</returns>
+    static int Main(string[] args)
+    {
+        return WriteMessage(Now, Console.Out);
+    }
 
-public static int writeMessage(DateTime CurrentDate, TextWriter writer)
-{
-    var intRetVal = 0; const Int32 MESSAGE_COUNT = 5; for (int MyIndex = 0; MyIndex < MESSAGE_COUNT; MyIndex++)
-    /*********************/
-    /* greater than 2016 */
-    /*********************/
-    if (CurrentDate.Year > 2016) writer.WriteLine("Happy New Year!");
-    else
-                writer.WriteLine("Hello From " + CurrentDate.Year);
+    /// <summary>
+    /// Writes a new year message when the supplied date is past 2016.
+    /// </summary>
+    /// <param name="currentDate">The current date.</param>
+    /// <param name="writer">The textwriter that will be used to write the message.</param>
+    /// <returns>The integer 0.</returns>
+    public static int WriteMessage(DateTime currentDate, TextWriter writer)
+    {
+        int returnValue = 0;
+        const int messageCount = 5;
+        for (int myIndex = 0; myIndex < messageCount; myIndex++)
+        {
+            // This checks if the current year is greater than 2016.
+            if (currentDate.Year > 2016)
+            {
+                writer.WriteLine("Happy New Year!");
+            }
+            else
+            {
+                writer.WriteLine("Hello From " + currentDate.Year);
+            }
+        }
 
-
-    //--------------------return the result value----------------------------
-
-
-
-
-    return intRetVal;
+        // This returns the current value.
+        return returnValue;
     }
 }
-
