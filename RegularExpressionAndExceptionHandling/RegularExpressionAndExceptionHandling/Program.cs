365caf901533a9ad1921e9c96eea3cef7cdff086
Cass Smuk <charles.smuk@gmail.com>
2017-02-21 20:21:13 -0700

    Created a phone validator for acceptable U.S. phone numbers and tests to check valid phone numbers.

---
+++ RegularExpressionAndExceptionHandling/RegularExpressionAndExceptionHandling/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+
+namespace RegularExpressionAndExceptionHandling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine(Regex.Match("16306321796", @"^(?:\+?(1))?[-./ (]*(\d{3})?[-./ )]*(\d{3})[-./ ]*(\d{4})*$").Success);
+            //Console.ReadKey();
+        }
+    }
+}
