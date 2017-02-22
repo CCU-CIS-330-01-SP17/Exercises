686b99b1013ff489f557e9d40605642e848ab709
Chris <Chris@ChrisPC>
2017-02-21 17:21:33 -0700

    Completed the Regex And Exception Handling Coding Exercise detailed on the powerpoint.

---
+++ RegexExceptionHandling/RegexExceptionHandling/PhoneValidator.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Text.RegularExpressions;
+namespace RegexExceptionHandling
+{
+    /// <summary>
+    /// This class containes all methods used to validate phone numbers via regular expressions.
+    /// </summary>
+    public class PhoneValidator
+    {
+        /// <summary>
+        /// This method validates a single phone number using regular expressions.
+        /// </summary>
+        /// <param name="numberToValidate"></param>
+        /// <returns></returns>
+        public bool validatePhoneNumber(string numberToValidate)
+        {
+            // This regular expression validates 10 & 11 digit US telephone numbers.
+            Regex phoneNumberExpression7 =
+                new Regex(@"^(?:\+?1[-/. ]?)?\(?([0-9]{3})\)?[-/. ]?([0-9]{3})[-/. ]?([0-9]{4})$");
+
+            // This regular expression validates 7 digit US telephone numbers.
+            Regex phoneNumberExpression5 =
+                new Regex(@"^([0-9]{3})[-/. ]?([0-9]{4})$");
+
+            if (numberToValidate == null)
+            {
+                throw new ArgumentNullException();
+            }
+            else if (phoneNumberExpression7.IsMatch(numberToValidate) == false)
+            {
+                if (phoneNumberExpression5.IsMatch(numberToValidate) == false)
+                {
+                    return false;
+                }
+                else
+                {
+                    return true;
+                }
+            }
+            else
+            {
+                return true;
+            }
+        }
+    }
+};
---
+++ RegexExceptionHandling/UnitTestProject1/PhoneValidatorTests.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RegexExceptionHandling;
+namespace UnitTestProject1
+{
+    /// <summary>
+    /// Defines tests for the <see cref="PhoneValidator"/> class.
+    /// </summary>
+    [TestClass]
+    public class PhoneValidatorTests
+    {
+        /// <summary>
+        /// This method tests to see if the phone number validator correctly returns "true" for each of the valid US phone numbers in a list.
+        /// </summary>
+        [TestMethod]
+        public void ValidateCorrectNumbers()
+        {
+            string [] validPhoneNumbers = 
+                {
+                "4221356",
+                "422 1356",
+                "422.1356",
+                "422-1356",
+                "422/1356",
+                "3034221356",
+                "1(303)-422-1356",
+                "(303)-422-1356",
+                "+1-3034221356",
+                "303/422/1356",
+                "303.422.1356",
+                "1303.422/1356",
+                "14225664125"
+                };
+            foreach (string number in validPhoneNumbers)
+            {
+                bool validator = new PhoneValidator().validatePhoneNumber(number);
+                Console.WriteLine(number + " " + validator);
+                Assert.AreEqual(true, validator);
+            }
+        }
+
+        /// <summary>
+        /// This method tests to see if the phone number validator correctly returns "false" for each of the valid US phone numbers in a list.
+        /// </summary>
+        [TestMethod]
+        public void ValidateIncorrectNumbers()
+        {
+            string[] invalidPhoneNumbers =
+                {
+                " 4221356",
+                "42213562",
+                "3034221356 ",
+                "((30 3)-422.1356",
+                "+2-3034221356",
+                "303/422//1356",
+                "303.422..1356",
+                "1303  422/1356",
+                "142256641255"
+                };
+            foreach (string number in invalidPhoneNumbers)
+            {
+                bool validator = new PhoneValidator().validatePhoneNumber(number);
+                Console.WriteLine(number + " " + validator);
+                Assert.AreEqual(false, validator);
+            }
+        }
+
+        /// <summary>
+        /// This method tests to see if the phone number validator correctly throws a ArgumentNullException when a null value is used.
+        /// </summary>
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void ThrowCorrectException()
+        {
+            string nullPhoneNumber = null;
+            new PhoneValidator().validatePhoneNumber(nullPhoneNumber);
+        }
+    }
+}
