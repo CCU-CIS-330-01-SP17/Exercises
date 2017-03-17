eb4e60cfd7bd4f406538fb2a2d862945001fd813
Cass Smuk <charles.smuk@gmail.com>
2017-03-16 18:41:53 -0600

    Serialization Homework.

---
+++ Week9Serialization/Week9Serialization/Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week9Serialization
+{
+    public class Class1
+    {
+
+    }
+
+}
