21145208ae363de20d16df5223adec8162a4dd04
Chris <Chris@ChrisPC>
2017-03-21 18:48:44 -0600

    NetworkingCodingExcersise

---
+++ NetworkingCodingExcersise/NetworkingCodingExcersiseTests/ProgramTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Net;
+
+namespace NetworkingCodingExcersiseTests
+{
+    [TestClass]
+    public class ProgramTests
+    {
+        [TestMethod]
+        public void CorrectServerResponse()
+        {
+            string searchUrl = "http://localhost:8001/?x=16&y=55";
+            using (WebClient client = new WebClient())
+            {
+                int serverResponse = Convert.ToInt32(client.DownloadString(searchUrl));
+                Assert.AreEqual(71, serverResponse);
+            }
+        }
+    }
+}
