e5229a5a7f022d0805034e6f251145ab9d7678a2
Cass Smuk <charles.smuk@gmail.com>
2017-03-25 11:29:34 -0600

    Changes that have been made so far.

--- Week10NetworkingCodingExercise/Week10NetworkingCodingExercise/Program.cs
+++ Week10NetworkingCodingExercise/Week10NetworkingCodingExercise/Program.cs
@@ -19,34 +19,8 @@ namespace StreamsIONetworking
         /// <param name="args"></param>
         static void Main(string[] args)
         {
-            //UriDemo();
-            //HttpListenerDemo();
+            HttpListenerDemo();
         }
-
-        /// <summary>
-        /// For understanding how to use query strings.
-        /// </summary>
-        private static void UriDemo()
-        {
-            Console.WriteLine("Uri:");
-            Uri link = new Uri("https://msdn.microsoft.com/en-us/library/system.uri(v=vs.110).aspx?query=abc");
-            Console.WriteLine($"Scheme: {link.Scheme}");
-            Console.WriteLine($"Port: {link.Port}");
-            Console.WriteLine($"Host: {link.Host}");
-            Console.WriteLine($"PathAndQuery: {link.PathAndQuery}");
-
-            Console.WriteLine();
-            Console.WriteLine("UriBuilder:");
-            UriBuilder linkBuilder = new UriBuilder("http://localhost:8001?x=1&y=2");
-            Console.WriteLine($"Scheme: {linkBuilder.Scheme}");
-            Console.WriteLine($"Port: {linkBuilder.Port}");
-            Console.WriteLine($"Host: {linkBuilder.Host}");
-            Console.WriteLine($"Path: {linkBuilder.Path}");
-            Console.WriteLine($"Query: {linkBuilder.Query}");
-
-            Console.ReadKey();
-        }
-
         /// <summary>
         /// Runs localhost based on user specified port number.
         /// </summary>
@@ -57,43 +31,8 @@ namespace StreamsIONetworking
             int portNumber = Convert.ToInt32(portNum);
             UriBuilder uri = new UriBuilder("http", "localhost", portNumber);
 
-            Console.WriteLine($"Query: {uri.Query}");
 
-            if (uri.Query != "")
-            {
-                ListenAsync(uri);
-            }
-            else
-            {
-                // Create a listener.
-                using (HttpListener listener = new HttpListener())
-                {
-                    listener.Prefixes.Add(uri.ToString());
-                    listener.Start();
-
-                    Console.WriteLine("Listening...");
-
-                    // Note: The GetContext method blocks while waiting for a request. 
-                    HttpListenerContext context = listener.GetContext();
-                    HttpListenerRequest request = context.Request;
-
-                    // Obtain a response object.
-                    HttpListenerResponse response = context.Response;
-
-                    // Construct a response.
-                    string responseString = "<HTML><BODY> Hello world!</BODY></HTML>";
-                    byte[] buffer = System.Text.Encoding.UTF8.GetBytes(responseString);
-
-                    // Get a response stream and write the response to it.
-                    response.ContentLength64 = buffer.Length;
-                    Stream output = response.OutputStream;
-                    output.Write(buffer, 0, buffer.Length);
-
-                    // You must close the output stream.
-                    output.Close();
-                    listener.Stop();
-                }
-            }
+            ListenAsync(uri);
 
             Console.WriteLine($"Server running on port {portNumber}. Press Enter to stop.");
             Console.ReadLine();
@@ -102,7 +41,7 @@ namespace StreamsIONetworking
         /// <summary>
         /// 
         /// </summary>
-        /// <param name="url"></param>
+        /// <param name="url">URI that is passed into so that HttpListener knows where to listen.</param>
         async static void ListenAsync(UriBuilder url)
         {
             UriBuilder uri = url;
@@ -134,9 +73,30 @@ namespace StreamsIONetworking
             string x = context.Request.QueryString["x"];
             string y = context.Request.QueryString["y"];
 
-            if (!string.IsNullOrWhiteSpace(x) && !string.IsNullOrWhiteSpace(y))
+            if (x == null && y == null)
             {
-                string result = x + y;
+                // Obtain a response object.
+                HttpListenerResponse response = context.Response;
+                // Obtain a response object.
+                string responseStringDefault = "<HTML><BODY>Hello world!</BODY></HTML>";
+                byte[] bufferDefault = System.Text.Encoding.UTF8.GetBytes(responseStringDefault);
+
+                // Get a response stream and write the response to it.
+                response.ContentLength64 = bufferDefault.Length;
+                Stream outputDefault = response.OutputStream;
+                outputDefault.Write(bufferDefault, 0, bufferDefault.Length);
+
+                // You must close the output stream.
+                outputDefault.Close();
+            }
+
+            int queryValueX = 0;
+            int queryValueY = 0;
+         
+
+            if (int.TryParse(x, out queryValueX) && int.TryParse(y, out queryValueY))
+            {
+                int result = queryValueX + queryValueY;
 
                 context.Response.ContentLength64 = Encoding.UTF8.GetByteCount(result.ToString());
                 context.Response.StatusCode = (int)HttpStatusCode.OK;
@@ -154,12 +114,24 @@ namespace StreamsIONetworking
 
                 // You must close the output stream.
                 output.Close();
-
-                Console.WriteLine(result);
-
             }
+
             else
             {
+                // Obtain a response object.
+                HttpListenerResponse response = context.Response;
+                string responseString = "<HTML><BODY>Please enter an integer value. </BODY></HTML>";
+                byte[] buffer = System.Text.Encoding.UTF8.GetBytes(responseString);
+
+                // Get a response stream and write the response to it.
+                response.ContentLength64 = buffer.Length;
+                Stream output = response.OutputStream;
+                output.Write(buffer, 0, buffer.Length);
+
+                // You must close the output stream.
+                output.Close();
+
+
                 context.Response.StatusCode = (int)HttpStatusCode.BadRequest;
                 context.Response.OutputStream.Close();
             }
---
+++ Week10NetworkingCodingExercise/Week10NetworkingCodingExerciseTest/UnitTest1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Week10NetworkingCodingExerciseTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            //use WebClient to pull information down and test against
+        }
+    }
+}
