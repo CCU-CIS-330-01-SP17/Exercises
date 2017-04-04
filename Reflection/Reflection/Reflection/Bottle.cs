7a772799c8a9062c0fc8b8a859b73a7118fc47db
Brennan McElhanon <bmcelhanon@students.ccu.edu>
2017-04-04 13:10:05 -0600

    Added Reflection classes and related tests, which acccess the class objects via reflection.

---
+++ Reflection/Reflection/Reflection/Bottle.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reflection
+{
+    /// <summary>
+    /// A container used for storing liquids.
+    /// </summary>
+    public class Bottle
+    {
+        /// <summary>
+        /// This bool is True if the bottle is full.
+        /// </summary>
+        public bool IsFull { get; set; }
+
+        /// <summary>
+        /// The Liquid continaed in the bottle.
+        /// </summary>
+        public string Liquid { get; set; }
+
+
+        /// <summary>
+        /// Fills the bottle with the specified liquid.
+        /// </summary>
+        public string Fill()
+        {
+            if (IsFull)
+            {
+                return "The liquid spills all over the floor...";
+            }
+            else
+            {
+                IsFull = true;
+                return $"You have filled the bottle with {Liquid}!";
+            }
+        }
+
+    }
+}
---
+++ Reflection/Reflection/Reflection/Stamp.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reflection
+{
+    /// <summary>
+    /// An object used to place ink or other patterns on a medium.
+    /// </summary>
+    public class Stamp
+    {
+        /// <summary>
+        /// Wether 
+        /// </summary>
+        public bool IsInked { get; set; }
+
+        /// <summary>
+        /// The message the stamp was made to print.
+        /// </summary>
+        public string Message { get; set; }
+
+
+        /// <summary>
+        /// Stamps the Message in the console if the stamp is inked.
+        /// </summary>
+        public string StampMessage()
+        {
+            return IsInked ? Message : "";
+        }
+
+    }
+}
