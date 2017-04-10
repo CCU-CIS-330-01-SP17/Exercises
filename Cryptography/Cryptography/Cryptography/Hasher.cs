7835a6be45a123b163897722fdc3717a7f6b31a1
Brennan McElhanon <bmcelhanon@mcelhanon.com>
2017-04-10 10:40:08 -0600

    Added Cryptography classes and related tests.

---
+++ Cryptography/Cryptography/Cryptography/Hasher.cs
@@ -0,0 +1,33 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace Cryptography
+{
+    /// <summary>
+    /// Takes input and performs hashing operations on it.
+    /// </summary>
+    public static class Hasher
+    {
+        /// <summary>
+        /// Hashes the input string using SHA256.
+        /// </summary>
+        /// <param name="input">The string input to hash.</param>
+        /// <returns>The hashed string.</returns>
+        public static string Hash(string input)
+        {
+            var data = Encoding.UTF8.GetBytes(input);
+            var hash = SHA256.Create().ComputeHash(data);
+            return Encoding.UTF8.GetString(hash);
+        }
+
+        /// <summary>
+        /// Hashes the input bytes using SHA256
+        /// </summary>
+        /// <param name="input">The byte array input to hash.</param>
+        /// <returns>The hashed byte array.</returns>
+        public static byte[] Hash(byte[] input)
+        {
+            return SHA256.Create().ComputeHash(input);
+        }
+    }
+}
---
+++ Cryptography/Cryptography/CryptographyTests/SymmetricEncryptorTests.cs
@@ -0,0 +1,26 @@
+﻿using System.Security.Cryptography;
+using Cryptography;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CryptographyTests
+{
+    [TestClass]
+    public class SymmetricEncryptorTests
+    {
+        [TestMethod]
+        public void Can_Encrypt_Decrypt()
+        {
+            //Set up the encryption.
+            var aesCryptoServiceProvider = new AesCryptoServiceProvider();
+            var key = aesCryptoServiceProvider.Key;
+            var iv = aesCryptoServiceProvider.IV;
+            string plainText = "My Plaintext String!";
+
+            //Encrypt and decrypt.
+            var encryptedString = SymmetricEncryptor.EncryptString(plainText, key, iv);
+            string decryptedString = SymmetricEncryptor.DecryptString(encryptedString, key, iv);
+            
+            Assert.AreEqual(plainText, decryptedString);
+        }
+    }
+}
