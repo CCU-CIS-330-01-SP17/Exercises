faf3156d1f770c95a886cfb504a271cc41dabc13
Brennan McElhanon <bmcelhanon@mcelhanon.com>
2017-04-17 10:45:41 -0600

    Added WritingUtensil and Highlighter. Work in Progress.

---
+++ Disposable/Disposable/Disposable/Highlighter.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Disposable
+{
+    public class Highlighter : WritingUntensil
+    {
+        private bool _isDisposed;
+        private readonly ConsoleColor _highlightColor;
+        private readonly ConsoleColor _inkColor;
+        public Highlighter(ConsoleColor inkColor, ConsoleColor highlightColor) : base(inkColor)
+        {
+            _highlightColor = highlightColor;
+            _inkColor = inkColor;
+            _isDisposed = false;
+        }
+
+        /// <summary>
+        /// Whether messages written should also be highlighted.
+        /// </summary>
+        public bool HighlightMessages { get; set; }
+
+        /// <summary>
+        /// Writes a message with the highlighter.
+        /// </summary>
+        /// <param name="message">The message that should be written.</param>
+        public override void Write(string message)
+        {
+            if (_isDisposed)
+                throw new ObjectDisposedException(this + " has been disposed and is no longer available.");
+
+            Console.ResetColor();
+            if (HighlightMessages)
+                Console.BackgroundColor = _highlightColor;
+            Console.ForegroundColor = _inkColor;
+            Console.WriteLine(message);
+        }
+    }
+}
---
+++ Disposable/Disposable/Disposable/WritingUtensil.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace Disposable
+{
+    /// <summary>
+    /// A handheld object used for writing.
+    /// </summary>
+    public class WritingUntensil : IDisposable
+    {
+        private ConsoleColor _inkColor;
+        private bool _isDisposed;
+
+        /// <summary>
+        /// Creates a WritingUtensil
+        /// </summary>
+        /// <param name="inkColor">The color of the ink of the WritingUtensil.</param>
+        public WritingUntensil(ConsoleColor inkColor)
+        {
+            _inkColor = inkColor;
+            _isDisposed = false;
+        }
+
+        /// <summary>
+        /// Readies the WritingUtensil for disposal.
+        /// </summary>
+        public void Dispose()
+        {
+            Dispose(true);
+        }
+
+        /// <summary>
+        /// Readies the WritingUtensil for disposal and disables functionality.
+        /// </summary>
+        /// <param name="disposing">Specifies whether the object is in the process of disposing or not.</param>
+        public virtual void Dispose(bool disposing)
+        {
+            _isDisposed = true;
+        }
+
+        /// <summary>
+        /// Writes a message with the WritingUtensil.
+        /// </summary>
+        /// <param name="message">The message to write with the writing utensil.</param>
+        public virtual void Write(string message)
+        {
+            if (_isDisposed)
+                throw new ObjectDisposedException(this + " has been disposed and is no longer available.");
+
+            Console.ResetColor();
+            Console.ForegroundColor = _inkColor;
+            Console.WriteLine(message);
+        }
+    }
+}
