b0967c30f5ec0cb3b285b7c0769557b4d65540a8
Chris <Chris@ChrisPC>
2017-04-18 13:35:12 -0600

    Completed IDisposable excersie as detailed on the slides.

---
+++ IDisposableExercise/IDisposableExercise/Person.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.Win32.SafeHandles;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IDisposableExercise
+{
+    /// <summary>
+    /// Represents a person.
+    /// </summary>
+    public class Person : IDisposable
+    {
+        /// <summary>
+        /// Represents the name of a person.
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Flag: Has Disponse already been called?
+        /// </summary>
+        public bool disposed = false;
+        // Instantiate a SafeHandle instance.
+        SafeHandle handle = new SafeFileHandle(IntPtr.Zero, true);
+
+        /// <summary>
+        /// This method is a public implementation of Dispose callable by consumers.
+        /// </summary>
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        // Protected implementation of Dispose.
+        protected virtual void Dispose(bool disposing)
+        {
+            if (disposed)
+                return;
+            if (disposing)
+            {
+                handle.Dispose();
+            }
+            disposed = true;
+        }
+    }
+}
---
+++ IDisposableExercise/IDisposableExerciseTests/StudentTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using IDisposableExercise;
+
+namespace IDisposableExerciseTests
+{
+    /// <summary>
+    /// This class contains all tests for the <see cref="Student"/> class.
+    /// </summary>
+    [TestClass]
+    public class StudentTests
+    {
+        /// <summary>
+        /// This method checks to see if Dispose() is inplemented correctly in the derived class.
+        /// </summary>
+        [TestMethod]
+        public void StudentDisposeTest()
+        {
+            Student Maria = new Student();
+            Maria.Name = "Maria";
+            Maria.Grade = "12";
+            Maria.Dispose();
+            Assert.AreEqual(true, Maria.disposed);
+        }
+    }
+}
