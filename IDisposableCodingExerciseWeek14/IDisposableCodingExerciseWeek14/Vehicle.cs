a10b36ab85e3f4cb11d98392f63515631ecd6d68
Cass Smuk <charles.smuk@gmail.com>
2017-04-21 14:19:34 -0600

    Not my best work. Very confused with Disposal.

---
+++ IDisposableCodingExerciseWeek14/IDisposableCodingExerciseWeek14/Vehicle.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IDisposableCodingExerciseWeek14
+{
+    /// <summary>
+    /// Defines a vehicle and inherits from IDisposal interface.
+    /// </summary>
+    public class Vehicle :IDisposable
+    {
+        public string Name { get; set; }
+        /// <summary>
+        /// Constructs an instance of a vehicle.
+        /// </summary>
+        /// <param name="name">A variable used to name the vehicle.</param>
+        public Vehicle(string name)
+        {
+            Name = name;
+        }
+
+        void IDisposable.Dispose()
+        {
+        }
+    }
+}
